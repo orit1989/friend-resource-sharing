46d2de08b84da49b8e63bf536cec66cea02b818a
Saogat Rab <saogat.rab@icloud.com>
2018-04-29 16:38:16 -0400

    addded name to registration

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   // Getting references to our form and input
   var signUpForm = $("form.signup");
+  var firstName = $("input#firstName-input");
+  var lastName = $("input#lastName-input");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
@@ -8,23 +10,31 @@ $(document).ready(function() {
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
+      firstName: firstName.val().trim(),
+      lastName: lastName.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
-
+    console.log("User Data:");
+    console.log (userData);
     if (!userData.email || !userData.password) {
       return;
     }
-    // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    // If we have a first name and last name and email and password, run the signUpUser function
+    signUpUser(userData.firstName, userData.lastName, userData.email, userData.password);
+
+    firstName.val("");
+    lastName.val("");
     emailInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(firstName, lastName, email, password) {
     $.post("/api/signup", {
+      firstName: firstName,
+      lastName: lastName,
       email: email,
       password: password
     }).then(function(data) {
