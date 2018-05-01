ad0480376a4e20202c333a31c27bc7fd22a45e7f
Saogat Rab <saogat.rab@icloud.com>
2018-05-01 10:04:40 -0400

    test configs

--- models/index.js
+++ models/index.js
@@ -8,8 +8,12 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
+console.log( "ENV", env );
+console.log( "DIALECT", config.dialect );
+console.log( "ENV_VARIABLE", config.use_env_variable );
+
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], { dialect: config.dialect });
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
